d79007a825473a9f90308ac38d99dbe7f5d1fca3
WMCharles <charles.wafula@student.moringaschool.com>
2022-08-23 12:16:58 +0300

    v2.0

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-superbowlWin = (record) => {
-    const result = record.find(record => record.result === "W");
-    return result ? result.year : undefined;
-  }
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+
+function superbowlWin(array){
+    let y = array.find(x => x.result === "W")
+    if (y){
+        return y.year
+    }
+    else {
+        return undefined
+    }
+}
